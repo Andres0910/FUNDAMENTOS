02c9c15b49381d910b4b34d1a6f7ab1b914f7cf5
Andres0910 <andres.mejia.macias@utelvt.edu.ec>
2023-03-13 16:18:17 -0500

    Primer commit

---
+++ Trabajo_en_clase_1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using.namespace.std;
+int main()
+{
+	float a, b;
+	cin>>a>>b;
+	if (a==b)
+	{
+		cout<<"acceso permitido">>;
+	} else {
+		cout<<"acceso denegado">>;
+	}
+	return (0);
+}
---
+++ Trabajo_en_clase_3.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a, b;
+	printf ("Ingresar primera edad");
+	sacnf ("%d", &a);
+	printf ("Ingresar segunda edad");
+	scanf ("%d",&b)
+	if (a>b)
+	{
+		printf (a, "es mayor");	
+	} else if (a<b) {
+		printf(b "es mayor");
+	}
+	return(0);
+}
